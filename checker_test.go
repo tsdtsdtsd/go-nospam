c523a918793da0eafdcdc49c2d4857b4b7ac05e4
tsdtsdtsd <orkan.alat@googlemail.com>
2018-09-23 02:53:11 +0200

    Simple table test

--- checker_test.go
+++ checker_test.go
@@ -7,51 +7,68 @@ import (
 	"github.com/tsdtsdtsd/go-nospam/internal/user"
 )
 
+type userDataCheck struct {
+	Description string
+	Data        user.Data
+	ShouldScore float32
+}
+
+var userDataChecks = []userDataCheck{
+
+	userDataCheck{
+		Description: "BBCode spam",
+		ShouldScore: 1,
+		Data: user.Data{
+			Name:  "Peter Griffin",
+			Email: "peter@griffinmail.fun",
+			Text:  "[url=www.thingy156.net]thing[/url]",
+			IP:    "192.168.10.15",
+		},
+	},
+	userDataCheck{
+		Description: "Valid comment",
+		ShouldScore: 0,
+		Data: user.Data{
+			Name:  "Peter Griffin",
+			Email: "peter@griffinmail.fun",
+			Text:  "Hehehehehehehehe",
+			IP:    "192.168.10.15",
+		},
+	},
+}
+
 func TestDev(t *testing.T) {
 
 	checker := nospam.NewCheckerWithConfig(
 		&nospam.Options{},
 	)
 
-	var expected float32 = 1
+	for _, check := range userDataChecks {
+		rating, err := checker.Evaluate(&check.Data)
+		if err != nil {
+			t.Error(err)
+		}
 
-	ud := user.Data{
-		Name:  "Peter Griffin",
-		Email: "peter@gmail.com",
-		Text:  "[url=www.thingy156.net]thing[/url]",
-		IP:    "192.168.10.15",
+		if rating != check.ShouldScore {
+			t.Errorf("%s: rating should be %f but is %f", check.Description, check.ShouldScore, rating)
+		}
 	}
 
-	rating, err := checker.Evaluate(&ud)
-	if err != nil {
-		t.Error(err)
-	}
-
-	if rating != expected {
-		t.Errorf("rating should be %f but is %f", expected, rating)
-	}
 }
 
 var (
 	rating float32
 )
 
-func BenchmarkDev(b *testing.B) {
+func BenchmarkCheckerEvaluate(b *testing.B) {
 
 	checker := nospam.NewCheckerWithConfig(
 		&nospam.Options{},
 	)
 
-	ud := user.Data{
-		Name:  "Peter Griffin",
-		Email: "peter@gmail.com",
-		Text:  "[url=www.thingy156.net]thing[/url]",
-		IP:    "192.168.10.15",
-	}
-
 	for i := 0; i < b.N; i++ {
 		var err error
-		rating, err = checker.Evaluate(&ud)
+		rating, err = checker.Evaluate(&userDataChecks[0].Data)
 
 		if err != nil {
 			b.Error(err)
